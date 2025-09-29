0839e08574612d54a760b4c8366dbe4800776b46
Olumayowa <ooolowomeye@gmail.com>
2025-09-28 20:43:43 -0500

    iterations to 6

--- experimental/algorithm/LAGraph_LouvainIS.c
+++ experimental/algorithm/LAGraph_LouvainIS.c
@@ -365,7 +365,7 @@ int LAGraph_LouvainIS(
     int iter = 0;
     double Q = 0;
     double gamma = 1;
-    int max_iter = 4;
+    int max_iter = 6;
     
     GRB_TRY(LAGraph_IsolateSets(&Miset, A, seed, msg));
     while (changed && iter < max_iter)
